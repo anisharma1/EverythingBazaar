63016f7971c3eba45d66ab7900bb187a2968b51e
anisharma1 <111197294+anisharma1@users.noreply.github.com>
2023-09-15 01:38:29 +0530

    Added email functionality

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -5,6 +5,8 @@ const { generateToken } = require("../config/jwtToken");
 const validateMongoDBId = require("../utils/validateMongodbid");
 const { generateRefreshToken } = require("../config/refreshToken");
 const jwt = require("jsonwebtoken");
+const sendMail = require("./emailCtrl");
+const crypto = require("crypto");
 
 //create a user
 const createUser = asyncHandler(async (req, res) => {
@@ -208,6 +210,50 @@ const updatePassword = asyncHandler(async (req, res) => {
 	}
 });
 
+//function to generate forgotten password tokens
+//      and send the reset password link
+const forgotPasswordToken = asyncHandler(async (req, res) => {
+	const { email } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new Error("user not found with email entered");
+	}
+	try {
+		const token = await user.createPasswordResetToken();
+		await user.save();
+		const resetURL = `Hi! Please follow this link to reset your password. This link is valid for 10 mins from now. <a href='http://localhost:3000/api/user/reset-password/${token}'>Click Here</>`;
+		const data = {
+			to: email,
+			text: "Hey User",
+			subject: "Forgot Password Link",
+			htm: resetURL,
+		};
+		sendMail(data);
+		res.json({ message: `Token generated successfully: ${token}` });
+	} catch (err) {
+		throw new Error(err);
+	}
+});
+
+//function to reset the password using email
+const resetPassword = asyncHandler(async (req, res) => {
+	const { password } = req.body;
+	const token = req.params.token;
+	const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+	const user = await User.findOne({
+		passwordResetToken: hashedToken,
+		passwordResetExpires: { $gt: Date.now() },
+	});
+	if (!user) {
+		throw new Error("Token expired. Please try again later.");
+	}
+	user.password = password;
+	user.passwordResetToken = undefined;
+	user.passwordResetExpires = undefined;
+	await user.save();
+	res.json(user);
+});
+
 module.exports = {
 	createUser,
 	loginUserCtrl,
@@ -220,4 +266,6 @@ module.exports = {
 	handleRefreshToken,
 	logout,
 	updatePassword,
+	forgotPasswordToken,
+	resetPassword,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,90 +1,92 @@
-const mongoose = require('mongoose'); // Erase if already required
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
+const mongoose = require("mongoose"); // Erase if already required
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    lastname:{
-        type:String,
-        required:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    role:{
-        type: String,
-        default: "user"
-    },
-    isBlocked: {
-        type: Boolean,
-        default: false
-    },
-    cart: {
-        type: Array,
-        default: []
-    },
-    address: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "Address"
-    },
-    wishlist: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "Product"
-    },
-    refreshToken: {
-        type: String,
-    },
-    passwordChangedAt: Date,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-}, {
-    timestamps: true // Enable automatic timestamps
-});
+var userSchema = new mongoose.Schema(
+	{
+		firstname: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true,
+		},
+		lastname: {
+			type: String,
+			required: true,
+			index: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		mobile: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			default: "user",
+		},
+		isBlocked: {
+			type: Boolean,
+			default: false,
+		},
+		cart: {
+			type: Array,
+			default: [],
+		},
+		address: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Address",
+		},
+		wishlist: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Product",
+		},
+		refreshToken: {
+			type: String,
+		},
+		passwordChangedAt: Date,
+		passwordResetToken: String,
+		passwordResetExpires: Date,
+	},
+	{
+		timestamps: true, // Enable automatic timestamps
+	}
+);
 
 //encrypts the password after entry is saved
-userSchema.pre("save", async function(next) {
-    if (!this.isModified('password')) {
-        next() ;
-    }
-    const salt = bcrypt.genSaltSync(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) {
+		next();
+	}
+	const salt = bcrypt.genSaltSync(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
 });
 
 //method to match passwords
-userSchema.methods.isPasswordMatched = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-}
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 
 //method to create PasswordResetToken
-userSchema.methods.createPasswordResetToken = async function() {
-    const resetToken = crypto.randomBytes(32).toString("hex");
-    this.passwordResetToken = crypto
-        .createHash("sha256")
-        .update(resetToken)
-        .digest("hex");
-    this.passwordResetExpires = Date.now()+60*600*60*1000; //10 mins
-    return resetToken;
-}
-
+userSchema.methods.createPasswordResetToken = async function () {
+	const resetToken = crypto.randomBytes(32).toString("hex");
+	this.passwordResetToken = crypto
+		.createHash("sha256")
+		.update(resetToken)
+		.digest("hex");
+	this.passwordResetExpires = Date.now() + 60 * 10000; //10 mins
+	return resetToken;
+};
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
